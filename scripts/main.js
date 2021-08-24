55ac051342ec850d49cd905a4f71fb5da01d7339
Humoyun <87017227+HumoyunDeveloper@users.noreply.github.com>
2021-08-24 14:46:35 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -161,7 +161,7 @@ btn.onclick = function () {
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js').then(function (registration) {
+        navigator.serviceWorker.register('./sw.js').then(function (registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function (err) {
